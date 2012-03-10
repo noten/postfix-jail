fe8463efef698f30d8e5f6319a6e6f4533a7def8
noten <naoto@ic-tact.co.jp>
2012-03-10 12:13:00 +0900

    fixed username and added scripts.

---
+++ bin/mailjail2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+#/etc/postfix/master.cf
+#mailjail unix - n n - - pipe
+#  flags=FDRq user=_user_ argv=/home/_user_/mailjail/mailjail2.py $sender $recipient
+#
+
+import sys,os
+import time
+
+
+DIR=os.path.abspath(os.path.dirname(__file__))+"/mail2"
+
+if False == os.path.isdir(DIR):
+   os.mkdir(DIR)
+
+def main():
+  (sender,receipient)=('from','to')
+  if len(sys.argv) > 1:
+    sender=sys.argv[1]
+    if len(sys.argv)> 2:
+       receipient = sys.argv[2]
+  filename = sender + "-" + receipient+'-'+time.strftime('%Y%m%dT%H%M%S',time.localtime()) + ".eml"
+
+  if sys.stdin.isatty():
+    print "*** NO STDIN ***"
+    return
+
+  f=open(DIR+'/'+filename,'w')
+  if None != f:
+    f.writelines(sys.stdin.readlines())
+    f.close()
+
+
+if __name__ == '__main__':
+  main()
